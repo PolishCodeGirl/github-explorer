dac945ac62b0386250f02b4a2d67cb23ac0fb06f
PolishCodeGirl <malachowska.marzena@gmail.com>
2020-09-23 21:07:22 +0200

    Add UserTile component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 
 import Form from "./components/Form";
+import UserTile from "./components/UserTile";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Form />
+        <UserTile title="lalalal" description="blab;ab;ab" stars={34} />
+        <UserTile title="lalalal" description="blab;ab;ab" stars={24} />
       </header>
     </div>
   );
---
+++ src/components/UserTile.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+import Div from "styled-kit/Div";
+
+const UserTile = ({ title, description, stars }) => (
+  <Wrapper mTop={5} justifyBetween>
+    <Div column itemsStart>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+    </Div>
+    <Title style={{ fontSize: 14 }}>{`${stars} ★`}</Title>
+  </Wrapper>
+);
+
+export default UserTile;
+
+const Wrapper = styled(Div)`
+  width: 200px;
+  padding: 15px 5px;
+  background-color: gray;
+
+  p {
+    margin: 0;
+  }
+`;
+
+const Title = styled.p`
+  font-size: 16px;
+  font-weight: bold;
+`
+
+const Description = styled.p`
+  font-size: 14px;
+`;
